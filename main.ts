1f524dacac9c9ab45a9e97843375bca48a1eba8e
2023h <82404480+2023h@users.noreply.github.com>
2021-05-26 16:25:23 -0400

    started boss level

--- main.ts
+++ main.ts
@@ -1,31 +1,13 @@
 namespace SpriteKind {
     export const princess = SpriteKind.create()
+    export const princesss = SpriteKind.create()
+    export const guard = SpriteKind.create()
 }
 function garlicfarm () {
     tiles.setTilemap(tilemap`level3`)
     tiles.placeOnRandomTile(mySprite, assets.tile`myTile10`)
     controller.player1.moveSprite(mySprite, 100, 100)
-    mySprite.ay = 0
-    garlic_princess = sprites.create(img`
-        . . . f 4 4 f f f f . . . . . . 
-        . . f 4 5 5 4 5 f b f f . . . . 
-        . . f 5 5 5 5 4 e 3 3 b f . . . 
-        . . f e 4 4 4 e 3 3 3 3 b f . . 
-        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
-        . f 3 3 e e 3 b e 3 3 3 3 f . . 
-        . f 3 3 e e e f f 3 3 3 3 f . . 
-        . f 3 e e e f b f b b b b f . . 
-        . . f e 4 4 f 1 e b b b b f . . 
-        . . . f 4 4 4 4 f b b b b f f . 
-        . . . f e e e f f f b b b b f . 
-        . . . f d d d e 4 4 f b b f . . 
-        . . . f d d d e 4 4 e f f . . . 
-        . . f b d b d b e e b f . . . . 
-        . . f f 1 d 1 d 1 d f f . . . . 
-        . . . . f f b b f f . . . . . . 
-        `, SpriteKind.princess)
     garlic_count = 0
-    tiles.placeOnRandomTile(garlic_princess, assets.tile`myTile11`)
     for (let index = 0; index < 40; index++) {
         garlic = sprites.create(img`
             . . . . . . . . . . f f f f f . 
@@ -61,10 +43,62 @@ scene.onHitWall(SpriteKind.Player, function (sprite, location) {
         jump += -1
     }
 })
-sprites.onOverlap(SpriteKind.Player, SpriteKind.princess, function (sprite, otherSprite) {
-    game.showLongText("Hi! I am the garlic princess. The vampire apocalypse is destroying the world. To stop the vampires you must make spears out of garlic. Bring me 30 garlics and I will forge you a garlic spear.", DialogLayout.Full)
-    garlic_princess.destroy()
-})
+function battle () {
+    mySprite.setImage(img`
+        . . e . . . . . . . . . . 
+        . e d f f f f f f . . . . 
+        . d e f f f f f f f . . . 
+        . d d f f f f c f f f . . 
+        f e d f c f f f c f f f . 
+        f d d f c c f f f c c f f 
+        f d e f f f f e f f f f f 
+        d d d d f f f e e f f f . 
+        d d d d f b f e e f f f . 
+        e d d d e 1 f 4 4 f f . . 
+        . d d d e 4 4 4 4 f . . . 
+        . d e 4 e e e e f f . . . 
+        . d d 4 e 7 7 7 7 f . . . 
+        . d d e f 6 6 6 6 f f . . 
+        . f f f f f f f f f f . . 
+        . . f f . . . f f f . . . 
+        `)
+    vampire2 = sprites.create(img`
+        .f.......f....
+        .ff.....ffb...
+        fffcc.ccfbcb..
+        fffcccccfbcc..
+        .fc3cc3bcbccc.
+        .cb3bc3bbcccc.
+        ccbbbbbbbbcc..
+        c11bbb11bbbfc.
+        fbbbbbbbbbbfcc
+        fbcbbbcbbbbf..
+        .f1fff1bbbcf..
+        ..fbbbbbbcf...
+        ...fffffff....
+        ...fcccccbf...
+        ..ffbbbbfbbc..
+        .ffccdddcbbf..
+        ffccfdccffbc..
+        ccbbfddccfbc..
+        cbffbbbccfbf..
+        bbfbbbbbcfbfc.
+        .fcbbbbccbbbc.
+        .fbbbbbcccbbf.
+        ..bbbbbcccbbf.
+        ..fcbbbccf....
+        ..fcbfbbcf....
+        ..fbbfbbcf....
+        ..fccfbccf....
+        ..fccfbcbf....
+        ...ff..fff....
+        `, SpriteKind.Player)
+    tiles.placeOnRandomTile(vampire2, assets.tile`myTile15`)
+    tiles.placeOnRandomTile(mySprite, assets.tile`myTile12`)
+    tiles.setTilemap(tilemap`level5`)
+    mySprite.ay = 200
+    controller.player1.moveSprite(mySprite, 100, 0)
+}
 function vampire () {
     tiles.setTilemap(tilemap`level2`)
     tiles.placeOnRandomTile(mySprite, assets.tile`myTile10`)
@@ -76,15 +110,14 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
     otherSprite.destroy()
     garlic_count += 1
     if (30 < garlic_count) {
-        game.splash("go speak to the garlic princess")
-        garlic.destroy()
-        tiles.placeOnRandomTile(garlic_princess, assets.tile`myTile13`)
+        game.splash("you have forged your garlic spear")
+        battle()
     }
 })
+let vampire2: Sprite = null
 let jump = 0
 let garlic: Sprite = null
 let garlic_count = 0
-let garlic_princess: Sprite = null
 let mySprite: Sprite = null
 scene.setBackgroundImage(img`
     cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
@@ -227,7 +260,9 @@ mySprite = sprites.create(img`
     . f f f f f f f f f f . . 
     . . f f . . . f f f . . . 
     `, SpriteKind.Player)
-vampire()
 scene.cameraFollowSprite(mySprite)
 controller.player1.moveSprite(mySprite, 100, 0)
-mySprite.ay = 200
+vampire()
+forever(function () {
+    mySprite.ay = 200
+})
